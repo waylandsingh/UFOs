0a600ad40c7ae30f92c55eb88b858cdb86413582
waylandsingh <wmsingh@ucdavis.edu>
2022-02-04 14:34:21 -0800

    adjust html and add table building function

---
+++ static/js/app.js
@@ -0,0 +1,23 @@
+//import data from data.js
+const tableData = data
+
+// Reference to HTML table using d3
+var table = d3.select("tbody")
+
+let buildTable = (data) => {
+    // clear out the table by referencing the HTML container obj tbody
+    tbody.html("")
+
+    // loop through the data to populate the table
+    data.forEach(dataRow => {
+        // append new row to table
+        let row = tbody.append("tr");
+
+        // loop through the values for each column in the row
+        Object.values(dataRow).forEach(val => {
+            let cell = row.append("td");
+            cell.text(val);
+        });
+        
+    });
+}
