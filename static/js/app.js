5d36c619866c108c48587043b3241592bdae5dbb
waylandsingh <wmsingh@ucdavis.edu>
2022-02-07 18:10:00 -0800

    begin adding button-filter option by date

--- static/js/app.js
+++ static/js/app.js
@@ -4,8 +4,7 @@ const tableData = data
 // Reference to HTML table using d3
 var tbody = d3.select("tbody")
 
-console.log(tbody.html)
-let buildTable = (data) => {
+function buildTable (data) {
     // clear out the table by referencing the HTML container obj tbody
     tbody.html("")
 
@@ -24,3 +23,21 @@ let buildTable = (data) => {
 }
 console.log(tableData)
 buildTable(tableData)
+
+function handleClick() {
+    // use d3 to grab date
+    let date = d3.select("#datetime").property("value")
+
+    // need to parse the date from d3 to match that in the data to filter
+    console.log(date)
+    
+    let filteredData = tableData // from GLOBAL scope!
+
+    if (date) {
+        filteredData = filteredData.filter(result => {result.datetime === date})
+    }
+
+    buildTable(filteredData)
+}
+
+document.getElementById("submit-form").addEventListener("click", handleClick)
