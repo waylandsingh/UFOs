46f47f51b051b85185562a4993ae9c274ff50c1a
waylandsingh <wmsingh@ucdavis.edu>
2022-02-20 18:46:08 -0800

    rewrote callback method for script loading as a promise

--- static/js/app.js
+++ static/js/app.js
@@ -8,10 +8,9 @@ function loadScript(src, callback) {
     script.src = src;
 
     // execute the callback function
-    script.onload= ()=>callback(script) ;
+    script.onload = () => callback(script) ;
     document.head.append(script);
 }
-
 
 function buildTable(data){
     // clear out the table by referencing the HTML container obj tbody
@@ -33,11 +32,29 @@ function buildTable(data){
 
 var tbody = ''
 // use the ajax script as example here with a callback to print to console
-loadScript("https://cdnjs.cloudflare.com/ajax/libs/d3/4.11.0/d3.js", script => {
-    console.log(`${script.src} has been loaded`);
-    // Reference to HTML table using d3
-    tbody = d3.select("tbody")
-    buildTable(data)
-});
+// loadScript("https://cdnjs.cloudflare.com/ajax/libs/d3/4.11.0/d3.js", script => {
+//     console.log(`${script.src} has been loaded`);
+//     // Reference to HTML table using d3
+//     tbody = d3.select("tbody")
+//     buildTable(data)
+// });
 
-document.getElementById("submit-form").addEventListener("click", handleClick)
+// Try rewriting the callback as a promise
+let loadScripts = src => new Promise((resolve, reject) => {
+            let script = document.createElement("script")
+            script.src = src
+            document.head.append(script)
+            script.onload = () => resolve("script loaded") //remember that this should be a function!
+        })
+
+loadScripts("https://cdnjs.cloudflare.com/ajax/libs/d3/4.11.0/d3.js")
+    .then(result => {
+        tbody = d3.select("tbody")
+        console.log(result)
+        buildTable(data)
+    })
+    .catch(error => console.log(error))
+
+
+
+// document.getElementById("submit-form").addEventListener("click", handleClick)
